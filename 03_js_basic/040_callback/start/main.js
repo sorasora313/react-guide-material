42fe55f71f7133b63d9ee2f5a0b292522891d593
sorasora313 <sky.313.sora@gmail.com>
2022-09-23 07:24:58 +0900

    Update main.js

--- 03_js_basic/040_callback/start/main.js
+++ 03_js_basic/040_callback/start/main.js
@@ -1,15 +1,11 @@
-function fn(number) {
+function print(fn){
+  const result = fn(2);
+  console.log(result);
+}
+
+function fn(number = 3) {
     return number * 2;
   }
   
-  console.log(fn(2));
-  console.log(fn(4));
-  
-  const fnArrow = number => {
-    console.log(number);
-    return number * 2;
-  };
-  
-  const fnArrowObj = number => ({ result: number * 2 })
-  
-  console.log(fnArrowObj(2));
+  debugger;
+  print(fn);
